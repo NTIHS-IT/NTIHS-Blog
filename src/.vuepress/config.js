f3d452ac5be6a1ea0407ece8d884e812fdabb8d4
xiaoxigua <aijdfajodwsdf@gmail.com>
2021-08-21 15:38:03 +0800

    add website meta

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -18,7 +18,49 @@ module.exports = {
   head: [
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    [
+      'meta',
+      {
+        'property': 'og:title',
+        'content': 'NTIHS-IT Blog'
+      }
+    ],
+    [
+      'meta',
+      {
+        'property': 'og:url',
+        'content': 'https://ntihs-it.github.io/NTIHS-Blog/'
+      }
+    ],
+    [
+      'meta',
+      {
+        'property': 'og:image',
+        'content': 'https://avatars.githubusercontent.com/u/87763641?s=200&v=4'
+      }
+    ],
+    [
+      'meta',
+      {
+        'property': 'og:description',
+        'content': '台南高工網頁社 Blog'
+      }
+    ],
+    [
+      'meta',
+      {
+        'property': 'og:site_name',
+        'content': 'NTIHS-IT'
+      }
+    ],
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "https://avatars.githubusercontent.com/u/87763641?s=200&v=4"
+      }
+    ],
   ],
   theme: 'reco',
   /**
@@ -26,8 +68,8 @@ module.exports = {
    *
    * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
    */
-  dest: "build",
-  base: "/NTIHS-Blog/",
+  dest: 'build',
+  base: '/NTIHS-Blog/',
   themeConfig: {
     repo: '',
     editLinks: false,
@@ -35,15 +77,15 @@ module.exports = {
     editLinkText: '',
     lastUpdated: false,
     noFoundPageByTencent: false,
-    type: "blog",
+    type: 'blog',
     blogConfig: {
       category: {
         location: 2,
-        text: "Category"
+        text: 'Category'
       },
       tag: {
         location: 3,
-        text: "Tag"
+        text: 'Tag'
       }
     },
     nav: [
@@ -52,9 +94,9 @@ module.exports = {
         link: 'https://github.com/NTIHS-IT/blog',
       },
       {
-        text: "TimeLine",
-        link: "/timeline/",
-        icon: "reco-date"
+        text: 'TimeLine',
+        link: '/timeline/',
+        icon: 'reco-date'
       },
     ],
     sidebar: {
@@ -71,34 +113,34 @@ module.exports = {
     },
     friendLink: [
       {
-        title: "xiao xigua",
-        desc: "GitHub link",
-        email: "aijdfajodwsdf@gmail.com",
-        link: "https://github.com/xiaoxigua-1",
-        logo: "https://avatars.githubusercontent.com/u/60529600?v=4",
+        title: 'xiao xigua',
+        desc: 'GitHub link',
+        email: 'aijdfajodwsdf@gmail.com',
+        link: 'https://github.com/xiaoxigua-1',
+        logo: 'https://avatars.githubusercontent.com/u/60529600?v=4',
       },
       {
-        title: "Young",
-        desc: "GitHub link",
-        emai: "young20050727@gmail.com",
-        link: "https://github.com/Young-TW",
-        logo: "https://avatars.githubusercontent.com/u/62501690?v=4",
+        title: 'Young',
+        desc: 'GitHub link',
+        emai: 'young20050727@gmail.com',
+        link: 'https://github.com/Young-TW',
+        logo: 'https://avatars.githubusercontent.com/u/62501690?v=4',
       },
       {
-        title: "zan zan",
-        desc: "GitHub link",
-        email: "Null",
-        link: "https://github.com/zanya945",
-        logo: "https://avatars.githubusercontent.com/u/77784793?v=4",
+        title: 'zan zan',
+        desc: 'GitHub link',
+        email: 'Null',
+        link: 'https://github.com/zanya945',
+        logo: 'https://avatars.githubusercontent.com/u/77784793?v=4',
       }
     ],
-    logo: "https://avatars.githubusercontent.com/u/87763641?s=200&v=4",
+    logo: 'https://avatars.githubusercontent.com/u/87763641?s=200&v=4',
     search: true,
     searchMaxSuggestions: 10,
-    lastUpdated: "Last Updated",
-    author: "NTIHS-IT",
-    authorAvatar: "https://avatars.githubusercontent.com/u/87763641?s=200&v=4",
-    startYear: "2021",
+    lastUpdated: 'Last Updated',
+    author: 'NTIHS-IT',
+    authorAvatar: 'https://avatars.githubusercontent.com/u/87763641?s=200&v=4',
+    startYear: '2021',
   },
 
   /**
