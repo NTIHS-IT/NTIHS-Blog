f6987999b471ed77e6af8732128de96ef8eda930
xiaoxigua <aijdfajodwsdf@gmail.com>
2021-08-21 13:47:07 +0800

    init

---
+++ src/.vuepress/config.js
@@ -0,0 +1,71 @@
+const { description } = require('../../package')
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'NTIHS-IT Blog',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+  theme: 'reco',
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    noFoundPageByTencent: false,
+    type: "blog",
+    nav: [
+      {
+        text: 'GitHub',
+        link: 'https://github.com/NTIHS-IT/blog',
+      }
+    ],
+    sidebar: {
+      '/guide/': [
+        {
+          title: 'Guide',
+          collapsable: false,
+          children: [
+            '',
+            'using-vue',
+          ]
+        }
+      ],
+    },
+    logo: "https://avatars.githubusercontent.com/u/87763641?s=200&v=4",
+    search: true,
+    searchMaxSuggestions: 10,
+    lastUpdated: "Last Updated",
+    author: "NTIHS-IT",
+    authorAvatar: "https://avatars.githubusercontent.com/u/87763641?s=200&v=4",
+    startYear: "2021",
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ],
+}
---
+++ src/index.md
@@ -0,0 +1,5 @@
+---
+home: true
+# heroImage: https://v1.vuepress.vuejs.org/hero.png
+tagline: 網頁社
+---
